72adb4a9ec1134663e8970997536e05d826238c9
Gabriel Schrock <gabriel@democracy.works>
2022-01-19 09:08:40 -0800

    user can sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::Base
+  def authenticate
+    redirect_to new_session_path unless signed_in?
+  end
+
+  # NOTE: alternate syntax
+  # def authenticate
+  #   if !signed_in?
+  #     redirect_to new_session_path
+  #   end
+  # end
+
+  def signed_in?
+    session[:current_email].present?
+  end
 end
